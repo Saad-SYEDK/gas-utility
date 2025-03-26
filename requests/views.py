ad49a13999167ec32966ab363a404715d9810f7c
Saad Syed Kaleemulla <saadsyedmd@gmail.com>
2025-03-26 15:40:28 +0530

    Created a view for logged-in user along wiht their service requsts

--- requests/views.py
+++ requests/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import ServiceRequest
 
 # Create your views here.
+
+@login_required
+def service_request_list(request):
+    requests = ServiceRequest.objects.filter(user=request.user)
+    return render(request, 'requests/service_requests.html', {'request': requests})
+
+
