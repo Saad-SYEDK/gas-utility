559a02dd0e9ebd5d2ff179e481f66ec5d1a2440b
Saad Syed Kaleemulla <saadsyedmd@gmail.com>
2025-03-27 20:20:59 +0530

    Register function working

--- services/urls.py
+++ services/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import include, path
 from requests import views  # Import your views
 
 urlpatterns = [
@@ -12,6 +12,7 @@ urlpatterns = [
     path("track/", views.track_requests, name="track_requests"),
     path("login/", auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("", include("accounts.urls")),
     
 ]
 
