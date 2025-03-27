440246a9f81f247c4c91b8ef0e76c0ef00622ad6
Saad Syed Kaleemulla <saadsyedmd@gmail.com>
2025-03-27 17:54:03 +0530

    setup accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import SignUpForm
+from django.contrib.auth.decorators import login_required
+from requests.models import ServiceRequest
 
-# Create your views here.
+# Signup View
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'accounts/signup.html', {'form': form})
