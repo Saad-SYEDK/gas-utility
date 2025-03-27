cb6c2f936170c3a3c8f340d549e477803a06798b
Saad Syed Kaleemulla <saadsyedmd@gmail.com>
2025-03-27 19:24:40 +0530

    Made changes in accounts/views.py - added register and account methods

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import login, authenticate
 from .forms import SignUpForm
 from django.contrib.auth.decorators import login_required
 from requests.models import ServiceRequest
+from django.contrib.auth.forms import UserCreationForm
 
 # Signup View
 def signup_view(request):
@@ -21,3 +22,18 @@ def signup_view(request):
 def user_dashboard(request):
     requests = ServiceRequest.objects.filter(user=request.user)
     return render(request, 'accounts/dashboard.html', {'requests': requests})
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log the user in after successful registration
+            return redirect("account")  # Redirect to account page
+    else:
+        form = UserCreationForm()
+    return render(request, "accounts/register.html", {"form": form})
+
+def account(request):
+    return render(request, "accounts/account.html")
+
