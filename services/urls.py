a92fd704356506d2566aacc86da74c1143918c5b
Saad Syed Kaleemulla <saadsyedmd@gmail.com>
2025-03-27 17:59:49 +0530

    Added new paths in services/urls.py - login, logout

--- services/urls.py
+++ services/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
@@ -9,6 +10,9 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("submit/", views.submit_request, name="submit_request"),
     path("track/", views.track_requests, name="track_requests"),
+    path("login/", auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    
 ]
 
 if settings.DEBUG:
