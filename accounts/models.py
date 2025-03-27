fabc7d85453611fe826550ddf3974189271e180b
Saad Syed Kaleemulla <saadsyedmd@gmail.com>
2025-03-27 17:52:40 +0530

    Added profile table in accounts/models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
+# accounts/models.py
+from django.contrib.auth.models import User
 from django.db import models
+from requests.models import ServiceRequest  # Importing request model
 
-# Create your models here.
+# Extend User model with profile info (if needed)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=15, blank=True)
